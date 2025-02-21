82e98f3fecd2bb4ec525698497e79f7302c03943
AnkitaIBM <ankitachawla@in.ibm.com>
2025-02-21 16:32:15 +0530

    Add files via upload

---
+++ Appium/Activities/Activity3.java
@@ -0,0 +1,192 @@
+package activity;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class Activity2 
+{
+    // Driver Declaration
+    AndroidDriver driver;
+    
+    // Set up method
+    @BeforeClass
+    public void setUp() throws MalformedURLException, URISyntaxException 
+    {
+        // Desired Capabilities
+        UiAutomator2Options options = new UiAutomator2Options();
+        options.setPlatformName("android");
+        options.setAutomationName("UiAutomator2");
+        options.setAppPackage("com.android.chrome");
+        options.setAppActivity("com.google.android.apps.chrome.Main");
+        options.noReset();
+
+        // Set the Appium server URL
+        URL serverURL = new URI("http://localhost:4723").toURL();
+
+        // Driver Initialization
+        driver = new AndroidDriver(serverURL, options);
+
+        // Open the page in Chrome
+        driver.get("https://training-support.net");
+    }
+
+    // Test method
+    @Test
+    public void chromeTest() throws InterruptedException 
+    {
+    	// Find heading on the page
+        String pageHeading = driver.findElement(AppiumBy.xpath("//android.view.View[@text=\"Training Support\"]")).getText();
+
+        Thread.sleep(5000);
+        
+        // Print to console
+        System.out.println("Page Heading: " + pageHeading);
+        
+        // Find and click the About Us link
+        driver.findElement(AppiumBy.xpath("//android.view.View[@text=\"About Us\"]")).click();
+
+        Thread.sleep(5000);
+        
+        // Find heading of new page and print to console
+        String aboutPageHeading = driver.findElement(AppiumBy.xpath("//android.view.View[@text=\"About Us\"]")).getText();
+        System.out.println("Page Heading: " + aboutPageHeading);
+    }
+
+
+    // Tear down method
+    @AfterClass
+    public void tearDown() 
+    {
+        // Close the app
+        driver.quit();
+    }
+}package activity;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class Activity3 
+{
+    // Driver Declaration
+    AndroidDriver driver;
+
+    // Set up method
+    @BeforeClass
+    public void setUp() throws MalformedURLException, URISyntaxException 
+    {
+        // Desired Capabilities
+        UiAutomator2Options options = new UiAutomator2Options();
+        options.setPlatformName("android");
+        options.setAutomationName("UiAutomator2");
+        options.setAppPackage("com.android.calculator2");
+        options.setAppActivity(".Calculator");
+        options.noReset();
+
+        // Server Address
+        URL serverURL = new URI("http://localhost:4723").toURL();
+
+        // Driver Initialization
+        driver = new AndroidDriver(serverURL, options);
+    }
+
+    // Test method
+    @Test
+    public void additionTest() 
+    {
+        // Perform the calculation
+        driver.findElement(AppiumBy.id("digit_5")).click();
+        driver.findElement(AppiumBy.accessibilityId("plus")).click();
+        driver.findElement(AppiumBy.id("digit_9")).click();
+        driver.findElement(AppiumBy.accessibilityId("equals")).click();
+
+        // Find the result
+        String result = driver.findElement(AppiumBy.id("result")).getText();
+
+        // Assertion
+        Assert.assertEquals(result, "14");
+    }
+
+    // Test method
+    @Test
+    public void subtractTest() 
+    {
+        // Perform the calculation
+        driver.findElement(AppiumBy.id("digit_1")).click();
+        driver.findElement(AppiumBy.id("digit_0")).click();
+        driver.findElement(AppiumBy.accessibilityId("minus")).click();
+        driver.findElement(AppiumBy.id("digit_5")).click();
+        driver.findElement(AppiumBy.accessibilityId("equals")).click();
+
+        // Find the result
+        String result = driver.findElement(AppiumBy.id("result")).getText();
+
+        // Assertion
+        Assert.assertEquals(result, "5");
+    }
+
+    // Test method
+    @Test
+    public void multiplyTest() 
+    {
+        // Perform the calculation
+        driver.findElement(AppiumBy.id("digit_5")).click();
+        driver.findElement(AppiumBy.accessibilityId("×")).click();
+        driver.findElement(AppiumBy.id("digit_1")).click();
+        driver.findElement(AppiumBy.id("digit_0")).click();
+        driver.findElement(AppiumBy.id("digit_0")).click();
+        driver.findElement(AppiumBy.accessibilityId("equals")).click();
+
+        // Find the result
+        String result = driver.findElement(AppiumBy.id("result")).getText();
+
+        // Assertion
+        Assert.assertEquals(result, "500");
+    }
+
+    // Test method
+    @Test
+    public void divideTest() 
+    {
+        // Perform the calculation
+        driver.findElement(AppiumBy.id("digit_5")).click();
+        driver.findElement(AppiumBy.id("digit_0")).click();
+        driver.findElement(AppiumBy.accessibilityId("divide")).click();
+        driver.findElement(AppiumBy.id("digit_2")).click();
+        driver.findElement(AppiumBy.accessibilityId("equals")).click();
+
+        // Find the result
+        String result = driver.findElement(AppiumBy.id("result")).getText();
+
+        // Assertion
+        Assert.assertEquals(result, "25");
+    }
+
+    // Tear down method
+    @AfterClass
+    public void tearDown() 
+    {
+        // Close the app
+        driver.quit();
+    }
+}
